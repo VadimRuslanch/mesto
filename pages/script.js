b34f0d3cd125a1ab6b6a0b6be7badc17421e4f29
VadimRuslanch <Merzlyy.Pes@yandex.ru>
2022-06-23 16:22:06 +0300

    Popup

--- pages/script.js
+++ pages/script.js
@@ -1,2 +1,19 @@
-let edit_button = document.querySelector(".profile__edit-button")
-let popup = document.querySelector(".popup")
+let main = document.querySelector(".main")
+
+let editButton = main.querySelector(".profile__edit-button")
+
+let popup = main.querySelector('.popup')
+let popupOpen = main.querySelector('.popup_opened')
+let closeButton = main.querySelector('.form__button-close')
+
+
+function closePopup(){
+    popup.classList.remove('popup_opened')
+}
+
+function openPopup(){
+    popup.classList.add('popup_opened')
+}
+
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopup);
